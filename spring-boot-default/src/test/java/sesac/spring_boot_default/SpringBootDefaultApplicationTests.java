60c4d3cc951e0dd686a7d78594e2e2e942645f06
ymind14563 <ymind14563@gmail.com>
2024-10-04 17:44:33 +0900

    study: Spring Boot 설정

---
+++ spring-boot-default/src/test/java/sesac/spring_boot_default/SpringBootDefaultApplicationTests.java
@@ -0,0 +1,13 @@
+package sesac.spring_boot_default;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDefaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
